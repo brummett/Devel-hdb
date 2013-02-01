6f39bb333180cfb015495bce835f0f78e7d9f97a
Anthony Brummett <brummett@gmail.com>
2013-01-31 23:14:06 -0600

    Add more control variables
    Include perl5db's trick fro maintining $DB::single even when multiple stack
    frames are unwound

--- lib/Devel/toydb.pm
+++ lib/Devel/toydb.pm
@@ -4,10 +4,20 @@ use strict;
 package Devel::toydb;
 
 package DB;
+no strict;
 
-our $stack_depth = 0;
-our $single = 0;
+BEGIN {
+    $DB::stack_depth    = 0;
+    $DB::single         = 0;
+    $DB::dbobj          = undef;
+    $DB::ready          = 0;
+    @DB::stack          = ();
+    $DB::deep           = 100;
+}
+
 sub DB {
+    return unless $ready;
+
     my($package, $filename, $line) = caller;
 
     print "In file $filename on line $line depth $stack_depth\n";
@@ -15,8 +25,21 @@ sub DB {
 }
 
 sub sub {
-    our $sub;
+    &$sub unless ($ready);
+
+    # Using the same trick perl5db uses to preserve the single step flag
+    # even in the cse where multiple stack frames are unwound, as in an
+    # an eval that catches an exception thrown many sub calls down
     local $stack_depth = $stack_depth + 1;
+    $#stack = $stack_depth;
+    $stack[-1] = $single;
+
+    # Turn off all flags except single-stepping
+    $single &= 1;
+
+    # If we've gotten really deeply recursed, turn on the flag that will
+    # make us stop with the 'deep recursion' message.
+    $single |= 4 if $stack_depth == $deep;
 
     print "Entering function $sub, stack depth $stack_depth\n";
     my(@ret,$ret);
@@ -33,10 +56,13 @@ sub sub {
         }
     }
 
-    $stack_depth--;
+    $single |= $stack[ $stack_depth-- ];
     print "Leaving function $sub, stack depth is now $stack_depth\n";
 
     return $wantarray ? @ret : $ret;
 }
+
+BEGIN { $DB::ready = 1; }
+END { $DB::ready = 0; }
 
 1;
