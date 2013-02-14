0a08344ce3c1d4b4f000ea906363df20379e022b
Anthony Brummett <brummett@gmail.com>
2013-02-13 22:11:14 -0600

    Allow specifying the port and testharness flag in import

--- lib/Devel/hdb.pm
+++ lib/Devel/hdb.pm
@@ -6,4 +6,16 @@ package Devel::hdb;
 use Devel::hdb::App;
 use Devel::hdb::DB;
 
+sub import {
+    my $class = shift;
+
+    while (@_) {
+        my $param = shift;
+        if ($param =~ m/port:(\d+)/) {
+            our $PORT = $1;
+        } elsif ($param eq 'testharness') {
+            our $TESTHARNESS = 1;
+        }
+    }
+}
 1;
