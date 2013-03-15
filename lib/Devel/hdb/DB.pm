84929b77783c2b968390f47c16b9a7d6abfd426f
Anthony Brummett <brummett@gmail.com>
2013-03-15 18:23:02 -0500

    Refactor to move the gash/glob eval expression fixup to a function

--- lib/Devel/hdb/DB.pm
+++ lib/Devel/hdb/DB.pm
@@ -326,14 +326,21 @@ sub prepare_eval {
     }
 }
 
+# This substitution is done so that we return HASH, as opposed to a list
+# An expression of %hash results in a list of key/value pairs that can't
+# be distinguished from a list.  A glob gets replaced by a glob ref.
+sub _fixup_expr_for_eval {
+    my($expr) = @_;
+
+    $expr =~ s/^\s*([%*])/\\$1/o;
+    return $expr;
+}
+
 sub eval {
 
     local($^W) = 0;  # no warnings
 
-    # This substitution is done so that we return HASH, as opposed to an ARRAY.
-    # An expression of %hash results in a list of key/value pairs.
-    # A glob gets replaced by a glob ref
-    $eval_string =~ s/^\s*([%*])/\\$1/o;
+    $eval_string = _fixup_expr_for_eval($eval_string);
 
     local @result;
     {
