0730ad21b60f7ab6d64a92879d2fec331632c615
Anthony Brummett <brummett@gmail.com>
2013-06-14 15:10:25 -0500

    documentation for Devel::hdb::DB::Stack

--- lib/Devel/hdb/DB/Stack.pm
+++ lib/Devel/hdb/DB/Stack.pm
@@ -146,5 +146,186 @@ __END__
 
 Devel::hdb::DB::Stack - An object representing the current execution stack
 
+=head1 SYNOPSIS
+
+  # Get the stack
+  my $stack = Devel::hdb::DB::Stack->new();
+  my $depth = $stack->depth();
+
+  # Get one particular stack frame
+  my $frame = $stack->frame(3);
+
+  # Iterate through the frames from most recent to oldest
+  my $iter = $stack->iterator();
+  while ($frame = $iter->()) {
+      print "Level "        . $frame->level
+          . " stopped in "  . $frame->subname
+          . " of package "  . $frame->package . "\n";
+  }
+
 =head1 DESCTIPTION
+
+The Stack object represents the current execution stack in the debugged
+program.  It encapsulates the information from the C<caller()> function,
+but differs from the raw caller data in that the stack frame locations
+reflect the currently executing line in each frame.  The difference is
+subtle, but tailored for what a debugger is likely interested in.
+
+The Stack object is composed of several L<Devel::hdb::DB::StackFrame>
+objects, one for each call frame.
+
+=head1 "TOP" AND "BOTTOM" OF THE STACK
+
+The "top" of the stack refers to the most recent call frame in the debugged
+program.  The "bottom" of the stack is the oldest frame, usually the main
+program not part of any function call.
+
+For example:
+
+  foo();                # bottom is here, frame 2
+
+  sub foo {
+      my @a = bar();    # Frame 1
+  }
+
+  sub bar {
+      $answer = 1 + 2;  # <-- debugger is stopped here
+  }
+
+If the debugger is stopped before executing the indicated line, the top of
+the stack would report line 8 in subroutine main::bar in array context.
+
+The call frame for the main program will look like this:
+  'hasargs'     1
+  subroutine    main::MAIN
+  package       main
+  args          @ARGV as it looks in a BEGIN block when the program starts
+  wantarray     0
+
+=head1 CONSTRUCTOR
+
+  my $stack = Devel::hdb::DB::stack->new();
+
+Returns an instance of Devel::hdb::DB::stack.  The call frames it contains
+does not include any stack frames within the debugger.
+
+=head1 METHODS
+
+=over 4
+
+=item $stack->depth()
+
+Returns the number of stack frames in the call stack
+
+=item $stack->frame($i)
+
+Return the $i-th call frame.  0 is the top of the stack.
+
+=item $stack->frames()
+
+Return a list of all the call frames.
+
+=item $stack->iterator()
+
+Returns a coderef to iterate through the call frames.  The top of the stack
+will be the first frame returned.  After the bottom frame is returned, the
+iterator will return undef.
+
+=back
+
+=head1 StackFrame METHODS
+
+These methods may be called on Devel::hdb::StackFrame instances:
+
+=over 4
+
+=item package
+
+The package this frame is in
+
+=item filename
+
+The filename this frame is in.  For string evals, this will be a string like
+"(eval 23)[/some/file/path:125]"
+
+=item line
+
+The line within the file for this frame.
+
+=item subroutine
+
+The full name of the subroutine for this frame.  It will include the pacakge.
+For the main program's stack frame, subroutine will be "main::MAIN".
+
+For an eval frame, subroutine will be "(eval)"
+
+=item hasargs
+
+True if this frame has its own instance of @_.  In practice, this will be false
+for eval frames, and for subroutines called as C<&subname;>, and true otherwise.
+
+=item wantarray
+
+The C<wantarray> context for this frame.  True if in array context, defined
+but false for scalar context, and undef for void context.
+
+=item evaltext
+
+For an eval frame, and it was a string eval, this will be the string that is
+eval-ed.  Non-string eval and other frames will have the value undef.
+
+=item is_require
+
+For an eval frame, this will be true if the frame is part of a "require" or
+"use".
+
+=item evalfile
+
+For a string eval frame, this is the file name the original string appeared
+in.  Other frames have the value undef.
+
+=item evalline
+
+For a string eval frame, this is the line the original string appeared
+in.  Other frames have the value undef.
+
+=item hints
+
+The hints value returned be C<caller> for this frame
+
+=item bitmask
+
+The bitmask value returned be C<caller> for this frame
+
+=item subname
+
+The subroutine name without the package name prepended to it.
+
+=item autoload
+
+If this call frame was entered because it was handled by an AUTOLOAD function,
+the 'autoload' attribute will be the function name that was actually called.
+The value is unreliable if called outside of the debugger system.
+
+=item level
+
+The number indicating how deep this call frame actually is.  This number is
+not relative to the program being debugged, and so reflects the real number
+of frames between the caller and the bottom of the stack, including any
+frames within the debugger.
+
+=back
+
+=head1 SEE ALSO
+
+L<Devel::hdb::DB>, L<Devel::StackTrace>
+
+=head1 AUTHOR
+
+Anthony Brummett <brummett@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright 2013, Anthony Brummett.  This module is free software. It may
+be used, redistributed and/or modified under the same terms as Perl itself.
 
