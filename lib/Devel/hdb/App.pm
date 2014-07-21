brummett/Devel-hdb

commit 44babb59c8761423aff086ab89b17db5e34dc768

Announced URL now matches the new API

lib/Devel/hdb/App.pm
@@ -128,7 +128,7 @@ sub _announce {
     } elsif ($hostname ne '127.0.0.1') {
         $hostname = gethostbyaddr($s->sockaddr, AF_INET);
     }
-    $self->{base_url} = sprintf('http://%s:%d/',
+    $self->{base_url} = sprintf('http://%s:%d/debugger-gui',
             $hostname, $s->sockport);
 
     select STDOUT;

commit 52fe917fbdb79dbb340cd08579da18e3f997cf6f

The announced URL isn't the same as base_url
This fixes the fork test that was broken by 44babb5

lib/Devel/hdb/App.pm
@@ -128,10 +128,12 @@ sub _announce {
     } elsif ($hostname ne '127.0.0.1') {
         $hostname = gethostbyaddr($s->sockaddr, AF_INET);
     }
-    $self->{base_url} = sprintf('http://%s:%d/debugger-gui',
+    $self->{base_url} = sprintf('http://%s:%d/',
             $hostname, $s->sockport);
 
-    STDOUT->printflush("Debugger pid $$ listening on ",$self->{base_url},"\n") unless ($Devel::hdb::TESTHARNESS);
+    my $announce_url = $self->{base_url} . 'debugger-gui';
+
+    STDOUT->printflush("Debugger pid $$ listening on $announce_url\n") unless ($Devel::hdb::TESTHARNESS);
 }
 
 sub on_notify_stopped {