5375647688af8b88433a7bb9c62e113c351ca13c
Anthony Brummett <brummett@gmail.com>
2014-12-08 15:05:21 -0600

    update to Handlebars 2.0.0
    Tried it out in an attempt to fix the rendering problem in #92, but it didn't
    help.  There's bound to be lots of bugfixes since the last update, so we'll keep it.

--- lib/Devel/hdb.pm
+++ lib/Devel/hdb.pm
@@ -243,7 +243,7 @@ This package includes these third party libraries:
 
 =item * Twitter Bootstrap version 2.3.0 L<http://twitter.github.com/bootstrap/index.html>
 
-=item * Handlebars version 1.0rc2 L<http://handlebarsjs.com/>
+=item * Handlebars version 2.0.0 L<http://handlebarsjs.com/>
 
 =back
 
